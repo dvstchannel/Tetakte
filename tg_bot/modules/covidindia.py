9aee0c7697972369488a97c15eda7f3d6c271043
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-07-02 01:34:04 +0530

    Update covidindia.py

--- tg_bot/modules/covidindia.py
+++ tg_bot/modules/covidindia.py
@@ -31,7 +31,7 @@ def covindia(bot: Bot, update: Update):
     if state:
         bot.send_message(
             message.chat.id,
-            '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
+            '*Covid case:*%s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
@@ -45,11 +45,11 @@ def covindia(bot: Bot, update: Update):
 
 __help__ = """
  
- - /covindia <state>: Get real time COVID-19 stats for the input Indian state
+ - /covind <state>: Get real time COVID-19 stats for the input Indian state
 """
 
-__mod_name__ = 'COVID-19 VIRUS'
+__mod_name__ = 'Corona'
 
-COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
+COV_INDIA_HANDLER = CommandHandler('covind', covind)
 
 dispatcher.add_handler(COV_INDIA_HANDLER)

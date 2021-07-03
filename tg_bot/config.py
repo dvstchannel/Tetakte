8d749940b4c317d7589b037188bffa4b4918884f
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-07-03 17:05:51 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -7,8 +7,7 @@ class Config(object):
     OWNER_USERNAME = "rsrmusic"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = ''  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    MESSAGE_DUMP = -1001465429519  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
